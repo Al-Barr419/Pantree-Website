3ffe9856c5a70017200e39ff54aa2c36fa0883e4
Al-Barr <20abka@queensu.ca>
2024-01-31 15:47:45 -0500

    fixed form styling etc

--- front-end/src/components/MailListForm.jsx
+++ front-end/src/components/MailListForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
@@ -10,46 +10,101 @@ const validationSchema = Yup.object().shape({
 });
 
 function MailListForm() {
+  const [submitted, setSubmitted] = useState(false); // New state for tracking submission status
+
   return (
     <Formik
       initialValues={{ first_name: "", last_name: "", email: "" }}
       validationSchema={validationSchema}
       onSubmit={(values, actions) => {
         axios
-          .post(`${process.env.BACKEND_URL}/add-contact`, {
+          .post(`${process.env.REACT_APP_BACKEND_URL}/add-contact`, {
             email: values.email,
             first_name: values.first_name,
             last_name: values.last_name,
           })
-          .then((response) => console.log(response.data))
-          .catch((error) => console.error("Error:", error));
+          .then((response) => {
+            console.log(response.data);
+            setSubmitted(true); // Update the state to indicate successful submission
+            actions.resetForm(); // Optional: reset the form fields
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+            actions.setSubmitting(false);
+          });
 
         console.log(values);
         actions.setSubmitting(false);
       }}
     >
       {({ isSubmitting }) => (
-        <Form>
-          <div>
-            <div>
-              <label htmlFor="first_name">First Name</label>
-              <Field name="first_name" type="text" />
-              <ErrorMessage name="first_name" component="div" />
-
-              <label htmlFor="last_name">Last Name</label>
-              <Field name="last_name" type="text" />
-              <ErrorMessage name="last_name" component="div" />
+        <>
+          {submitted ? (
+            <div className="text-center p-8 bg-[#DFF1FF] text-[#00566E] rounded-lg shadow-lg">
+              Thank you for joining the Waitlist!
             </div>
+          ) : (
+            <Form className="bg-[#F1FCFF] text-[#00566E] p-8 rounded-lg shadow-lg">
+              <div className="mb-6">
+                <label
+                  htmlFor="first_name"
+                  className="block font-bold text-lg mb-2"
+                >
+                  First Name
+                </label>
+                <Field
+                  name="first_name"
+                  type="text"
+                  className="w-full py-2 px-3 border rounded-lg"
+                />
+                <ErrorMessage
+                  name="first_name"
+                  component="div"
+                  className="text-red-500 mt-2"
+                />
 
-            <label htmlFor="email">Email</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" />
+                <label
+                  htmlFor="last_name"
+                  className="block font-bold text-lg mt-4 mb-2"
+                >
+                  Last Name
+                </label>
+                <Field
+                  name="last_name"
+                  type="text"
+                  className="w-full py-2 px-3 border rounded-lg"
+                />
+                <ErrorMessage
+                  name="last_name"
+                  component="div"
+                  className="text-red-500 mt-2"
+                />
+              </div>
 
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </div>
-        </Form>
+              <label htmlFor="email" className="block font-bold text-lg mb-2">
+                Email
+              </label>
+              <Field
+                type="email"
+                name="email"
+                className="w-full py-2 px-3 border rounded-lg"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500 mt-2"
+              />
+
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="mt-4 bg-[#00566E] text-white py-2 px-4 rounded-lg hover:bg-[#ddeeff]"
+              >
+                Submit
+              </button>
+            </Form>
+          )}
+        </>
       )}
     </Formik>
   );
--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
-import MailListForm from '../components/MailListForm';
+import React, { useState } from "react";
+import MailListForm from "../components/MailListForm";
 
 const Footer = () => {
   return (
     <footer className="bg-[#f1fcff] text-[#00566E] text-center min-h-[300px] flex flex-col justify-center">
-      <p className="text-lg leading-relaxed mb-6 font-anekTelugu">Receive notifications when your produce is about to expire.</p>
-      <p className="text-lg leading-relaxed mb-1 font-anekTelugu">No more losing track of ingredients.</p>
-      <p className="text-lg leading-relaxed mb-1 font-anekTelugu">No more food waste.</p>
+      <p className="text-lg leading-relaxed mb-6 font-anekTelugu">
+        Receive notifications when your produce is about to expire.
+      </p>
+      <p className="text-lg leading-relaxed mb-1 font-anekTelugu">
+        No more losing track of ingredients.
+      </p>
+      <p className="text-lg leading-relaxed mb-1 font-anekTelugu">
+        No more food waste.
+      </p>
     </footer>
   );
 };
@@ -19,20 +25,23 @@ const LandingPage = () => {
   };
 
   const overlayStyle = {
-    position: 'fixed',
+    position: "fixed",
     top: 0,
     left: 0,
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: '#F1FCFF',
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "#F1FCFF",
     zIndex: 1000,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   };
 
   return (
-    <div className='flex flex-col h-screen bg-[url("/Users/keelyjoon/Documents/coding/pantree_website/Pantree-Website/front-end/public/Fruits.png")] bg-cover bg-center bg-no-repeat'>
+    <div
+      className="flex flex-col h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url("/Fruits.png")` }}
+    >
       {showFormOnly && (
         <div style={overlayStyle}>
           <MailListForm />
@@ -40,17 +49,28 @@ const LandingPage = () => {
       )}
       {!showFormOnly && (
         <div className="flex flex-col flex-grow">
-          <div className='flex flex-col justify-center items-center flex-grow'>
+          <div className="flex flex-col justify-center items-center flex-grow">
             <img src="/Logo.png" alt="Pantree Logo" width={500} height={500} />
-            <p className="text-xl text-[#00566E] text-center pt-3.5 font-anekTelugu">Saving those blueberries you forgot about</p>
+            <p className="text-xl text-[#00566E] text-center pt-3.5 font-anekTelugu">
+              Saving those blueberries you forgot about
+            </p>
             <div className="flex justify-center gap-10 py-20">
-              <button className="text-5xl text-[#00536D] font-koulen bg-[#DFF4FA4D] rounded-3xl p-11 shadow-xl" onClick={handleButtonClick}>
-                JOIN WAITLIST<br />
-                <span className="text-2xl text-[#00536D] font-anekTelugu font-semibold">Your automatic fridge management app</span>
+              <button
+                className="text-5xl text-[#00536D] font-koulen bg-[#DFF4FA4D] rounded-3xl p-11 shadow-xl"
+                onClick={handleButtonClick}
+              >
+                JOIN WAITLIST
+                <br />
+                <span className="text-2xl text-[#00536D] font-anekTelugu font-semibold">
+                  Your automatic fridge management app
+                </span>
               </button>
               <button className="text-5xl text-[#00536D] font-koulen bg-[#DFF4FA4D] rounded-3xl p-11 shadow-xl">
-                ABOUT US<br />
-                <span className="text-2xl text-[#00536D] font-anekTelugu font-semibold" >Built by students</span>
+                ABOUT US
+                <br />
+                <span className="text-2xl text-[#00536D] font-anekTelugu font-semibold">
+                  Built by students
+                </span>
               </button>
             </div>
           </div>
