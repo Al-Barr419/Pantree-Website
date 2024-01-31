573b8d9307eea08c1422ab72b54d5e5834405f10
Keelyjoon <keelyjoon@gmail.com>
2024-01-31 08:50:09 -0500

    mail list form appears when button is clicked

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -1,33 +1,59 @@
-import React from "react";
-import MailListForm from "../components/MailListForm";
-// Import any other components or assets you need
+import React, { useState } from 'react';
+import MailListForm from '../components/MailListForm';
 
 const LandingPage = () => {
+  const [showFormOnly, setShowFormOnly] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowFormOnly(true);
+  };
+
+  const overlayStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'white',
+    zIndex: 1000, 
+    display: 'flex',
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  };
+
   return (
     <div className="landing-page">
-      <img src="/Logo.png" alt="Pantree Logo" className="logo" />
-      <div className="fruits-container">
-        <img src="/Fruits.png" alt="Fruits" className="fruits" />
-        <div className="text-overlay">
-          <p className="tagline">Saving those blueberries you forgot about</p>
+      {showFormOnly && (
+        <div style={overlayStyle}>
+          <MailListForm />
         </div>
-      </div>
-      <div className="buttons">
-        <button className="chrome-extension">
-          INSTALL CHROME EXTENSION<br />
-          <span className="button-subtext">Your automatic fridge management app</span>
-        </button>
-        <button className="about-us">
-          ABOUT US<br />
-          <span className="button-subtext">Built by students</span>
-        </button>
-      </div>
-      <ul className="benefits">
-        <li>Receive notifications when your produce is about to expire.</li>
-        <li>No more losing track of ingredients.</li>
-        <li>No more food waste.</li>
-      </ul>
-      <MailListForm />
+      )}
+      {!showFormOnly && (
+        <>
+          <img src="/Logo.png" alt="Pantree Logo" className="logo" />
+          <div className="fruits-container">
+            <img src="/Fruits.png" alt="Fruits" className="fruits" />
+            <div className="text-overlay">
+              <p className="tagline">Saving those blueberries you forgot about</p>
+            </div>
+          </div>
+          <div className="buttons">
+            <button className="chrome-extension" onClick={handleButtonClick}>
+              INSTALL CHROME EXTENSION<br />
+              <span className="button-subtext">Your automatic fridge management app</span>
+            </button>
+            <button className="about-us">
+              ABOUT US<br />
+              <span className="button-subtext">Built by students</span>
+            </button>
+          </div>
+          <ul className="benefits">
+            <li>Receive notifications when your produce is about to expire.</li>
+            <li>No more losing track of ingredients.</li>
+            <li>No more food waste.</li>
+          </ul>
+        </>
+      )}
     </div>
   );
 };
