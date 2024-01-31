e4a50821ca3906ec2148af00301ec251f2305859
Keelyjoon <keelyjoon@gmail.com>
2024-01-31 11:32:11 -0500

    using tailwind elements now

--- front-end/src/pages/LandingPage.jsx
+++ front-end/src/pages/LandingPage.jsx
@@ -14,45 +14,44 @@ const LandingPage = () => {
     left: 0,
     width: '100vw',
     height: '100vh',
-    backgroundColor: 'white',
-    zIndex: 1000, 
+    backgroundColor: '#rgba(241, 252, 255, 1)',
+    zIndex: 1000,
     display: 'flex',
-    alignItems: 'center', 
-    justifyContent: 'center', 
+    alignItems: 'center',
+    justifyContent: 'center',
   };
 
   return (
-    <div className="landing-page">
+    <div className='flex justify-center items-center h-screen'>
       {showFormOnly && (
         <div style={overlayStyle}>
           <MailListForm />
         </div>
       )}
       {!showFormOnly && (
-        <>
-          <img src="/Logo.png" alt="Pantree Logo" className="logo" />
-          <div className="fruits-container">
-            <img src="/Fruits.png" alt="Fruits" className="fruits" />
-            <div className="text-overlay">
-              <p className="tagline">Saving those blueberries you forgot about</p>
-            </div>
+        <div className="flex flex-col">
+          <div className='flex justify-center'>
+          <img src="/Logo.png" alt="Pantree Logo" width={400} height={400} />
           </div>
-          <div className="buttons">
-            <button className="chrome-extension" onClick={handleButtonClick}>
+          <p className="text-xl text-[#00566E] text-center pt-3.5 font-anekTelugu">Saving those blueberries you forgot about</p>
+          <div className="flex justify-center gap-10 py-20">
+            <div>
+            <button className="text-5xl text-[#00536D] font-koulen bg-[#DFF4FA4D] rounded-3xl p-11 shadow" onClick={handleButtonClick}>
               INSTALL CHROME EXTENSION<br />
-              <span className="button-subtext">Your automatic fridge management app</span>
+              <span className="text-2xl text-[#00536D] font-anekTelugu">Your automatic fridge management app</span>
             </button>
-            <button className="about-us">
+            </div>
+            <button className="text-5xl text-[#00536D] font-koulen  bg-[#DFF4FA4D] rounded-3xl p-11 shadow">
               ABOUT US<br />
-              <span className="button-subtext">Built by students</span>
+              <span className="text-2xl text-[#00536D] font-anekTelugu">Built by students</span>
             </button>
           </div>
-          <ul className="benefits">
+          {/* <ul className="benefits">
             <li>Receive notifications when your produce is about to expire.</li>
             <li>No more losing track of ingredients.</li>
             <li>No more food waste.</li>
-          </ul>
-        </>
+          </ul> */}
+        </div>
       )}
     </div>
   );
