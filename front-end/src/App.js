25b0d1e162ad5436206ba787f09ebcebff601645
Al-Barr <20abka@queensu.ca>
2024-02-23 00:49:36 -0500

    readded about-us route to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import LandingPage from "./pages/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Fridge from "./pages/Fridge";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/fridge" element={<Fridge />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </Router>
   );
