0040f377b24a9d1ce4bc4d582dd2169d0284940e
Al-Barr <20abka@queensu.ca>
2024-02-25 01:16:07 -0500

    checking if auth is the cause of deployment problem

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,10 @@
-import './App.css'
-import LandingPage from './pages/LandingPage'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Fridge from './pages/Fridge'
-import AboutUs from './pages/AboutUs'
-import Auth from './pages/Auth'
-import PrivacyPolicy from './pages/PrivacyPolicy'
+import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Fridge from "./pages/Fridge";
+import AboutUs from "./pages/AboutUs";
+// import Auth from './pages/Auth'
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 function App() {
   return (
     <Router className="App">
@@ -12,11 +12,11 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/fridge" element={<Fridge />} />
         <Route path="/about-us" element={<AboutUs />} />
-        <Route exact path="/auth" element={<Auth />} />
+        {/* <Route exact path="/auth" element={<Auth />} /> */}
         <Route exact path="/privacy-policy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
